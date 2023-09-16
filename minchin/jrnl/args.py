d7fb1a8f9e6e903c9c82ac56075225f06dbbc79a
MinchinWeb <w_minchin@hotmail.com>
2023-09-16 10:46:58 -0600

    Expand code licnese block
    with eye of moving back to MIT license

--- minchin/jrnl/args.py
+++ minchin/jrnl/args.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2012-2021 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
+# Code contributed on or before April 18, 2020 and by William Minchin (any
+# date) is duel licensed under the MIT license as well.
 
 import argparse
 import re
--- minchin/jrnl/install.py
+++ minchin/jrnl/install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Copyright (C) 2012-2021 jrnl contributors
 # License: https://www.gnu.org/licenses/gpl-3.0.html
+# Code contributed on or before April 18, 2020 and by William Minchin (any
+# date) is duel licensed under the MIT license as well.
 
 
 import glob
