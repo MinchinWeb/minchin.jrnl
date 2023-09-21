bd311d739b213ef66604ca0261373602ef9c7a07
MinchinWeb <w_minchin@hotmail.com>
2023-09-21 15:41:15 -0600

    [test] prototype unittest for JSON Exporter
    c.f. https://github.com/jrnl-org/jrnl/pull/1216/commits/c76b9788e6ba3ce0a8f1fd09b42af15d9873d711

---
+++ tests/test_plugin.py
@@ -0,0 +1,38 @@
+from datetime import date
+import json
+
+import pytest
+
+from minchin.jrnl import Entry, Journal
+from minchin.jrnl.plugins.exporter import json as json_exporter
+from minchin.jrnl.plugins.exporter import testing as testing_exporter
+
+
+@pytest.fixture()
+def create_entry():
+    entry = Entry.Entry(
+        journal=Journal.Journal(),
+        text="This is the entry text",
+        date=date(year=2001, month=1, day=1),
+        starred=True,
+    )
+    yield entry
+
+
+class TestBaseExporter(testing_exporter.Exporter):
+    def test_unimplemented_export(self, create_entry):
+        entry = create_entry
+        with pytest.raises(NotImplementedError):
+            self.export_entry(entry)
+
+
+class TestJsonExporter(json_exporter.Exporter):
+    def test_json_exporter_name(self):
+        assert "json" in self.names
+
+    def test_export_entry(self, create_entry):
+        entry = create_entry
+        exported = self.export_entry(entry)
+        deserialized_export = json.loads(exported)
+        assert deserialized_export["title"] == "This is the entry text"
+        assert deserialized_export["date"] == "2001-01-01"
